7c968f38f81973f38bcf62ceecf3c7ebfef604bb
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-09-12 19:43:20 +0530

    Update ReceiverCheck.cpp

--- Receiver/ReceiverCheck.cpp
+++ Receiver/ReceiverCheck.cpp
@@ -1,11 +1,42 @@
-#include<string>
-#include<cstring>
-#include<fstream>
-#include<sstream>
-#include<vector>
-#include<iterator>
-#include<algorithm>
 #include "ReceiverCheck.h"
 
 using namespace std;
 
+CSVReader reader("Testdata/dataset.csv");
+
+vector<float> CSVReader::temperatureFetch()
+{
+  vector<float> data;
+  ifstream file(fileName);
+  string line = "";
+  string temperature = "";
+  
+  while(getline(file,line))
+  {
+    stringstream str(line);
+    
+    getline(str,temperature,',');
+    
+    data.push_back(stof(temperature));
+  }
+  return data;
+}
+  
+vector<float> CSVReader::humidityFetch()
+{
+  vector<float> data;
+  ifstream file(fileName);
+  str line = "";
+  string humidity = "";
+  
+  while(getline(file,line))
+  {
+    stringstream str(line);
+    
+    getline(str,humidity,',');
+    getline(str,humidity,',');
+    
+    data.push_back(stof(humidity));
+  }
+  return data;
+}
