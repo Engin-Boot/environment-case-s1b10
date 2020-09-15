cf9221ef485705276d45008c4966be00de077b18
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-09-15 10:55:00 +0530

    Update ReceiverTest.cpp

--- Receiver/ReceiverTest.cpp
+++ Receiver/ReceiverTest.cpp
@@ -10,33 +10,31 @@ CSVReader reader("Testdata/dataset.csv");
 vector<float> temperature = reader.temperatureFetch();
 vector<float> humidity = reader.humidityFetch();
 
-int option = 0;
-
 TEST_CASE("Generate warning if Temperature >= 37 C or if Temperature <= 4 C")
 {
   obj.temperatureWarning(temperature);
   
-  REQUIRE(option == 1);
+  REQUIRE(obj.option == 1);
 }
 
 TEST_CASE("Generate error if Temperature >= 40 C or if Temperature <= 0 C")
 {
   obj.temperatureError(temperature);
   
-  REQUIRE(option == 2);
+  REQUIRE(obj.option == 2);
 }
 
 TEST_CASE("Generate warning if Humidity >= 70%")
 {
   obj.humidityWarning(humidity);
   
-  REQUIRE(option == 3);
+  REQUIRE(obj.option == 3);
 }
 
 TEST_CASE("Generate error if Humidity >= 90%")
 {
   obj.humidityError(humidity);
   
-  REQUIRE(option == 4);
+  REQUIRE(obj.option == 4);
 }
  
