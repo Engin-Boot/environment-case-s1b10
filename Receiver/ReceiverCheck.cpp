ebc5a64a9e19fcb3c3738c6d11c36ffc5c79a20d
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-09-15 22:42:57 +0530

    Update ReceiverCheck.cpp

--- Receiver/ReceiverCheck.cpp
+++ Receiver/ReceiverCheck.cpp
@@ -1,5 +1,4 @@
 #include "../Receiver/ReceiverCheck.h"
-#include "../Receiver/catch.hpp"
 using namespace std;
 
 CSVReader reader("Testdata/dataset.csv");
@@ -141,35 +140,5 @@ void ReceiverClass::humidityError(vector<float> humidity)
 
 int main()
 {
-  ReceiverClass obj;
-CSVReader reader("Testdata/dataset.csv");
-vector<float> temperature = reader.temperatureFetch();
-vector<float> humidity = reader.humidityFetch();
-TEST_CASE("Generate warning if Temperature >= 37 C or if Temperature <= 4 C")
-{
-  obj.temperatureWarningLow(temperature);
-  obj.temperatureWarningHigh(temperature);
-  
-  REQUIRE(obj.option == 1);
-}
-TEST_CASE("Generate error if Temperature >= 40 C or if Temperature <= 0 C")
-{
-  obj.temperatureErrorLow(temperature);
-  obj.temperatureErrorHigh(temperature);
-  
-  REQUIRE(obj.option == 2);
-}
-TEST_CASE("Generate warning if Humidity >= 70%")
-{
-  obj.humidityWarning(humidity);
-  
-  REQUIRE(obj.option == 3);
-}
-TEST_CASE("Generate error if Humidity >= 90%")
-{
-  obj.humidityError(humidity);
-  
-  REQUIRE(obj.option == 4);
-}
   return 0;
 }
