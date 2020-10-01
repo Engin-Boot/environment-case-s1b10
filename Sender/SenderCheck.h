749d5c7a75741c45bd33494eba82cf4e2adb4d02
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-10-01 14:56:37 +0530

    Update SenderCheck.h

--- Sender/SenderCheck.h
+++ Sender/SenderCheck.h
@@ -8,14 +8,14 @@ using namespace std;
  */
 class CSVReader
 {
-    string fileName;
-    string delimeter;
-public:
+    //string fileName;
+    //string delimeter;
+/*public:
     CSVReader(const string& filename, const string& delm = ",") :
             fileName(filename), delimeter(delm)
-    { }
+    { }*/
     // Function to fetch data from a CSV File
-    std::vector<std::vector<std::string> > getData();
+    std::vector<std::vector<std::string> > getData(string filename);
     vector<float> temperatureFetch();
     vector<float> humidityFetch();
 };
