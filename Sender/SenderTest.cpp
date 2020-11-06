57afc484c01645aea98d951d83436184f89c1043
ChinmitaShetty <69634891+ChinmitaShetty@users.noreply.github.com>
2020-11-06 13:19:38 +0530

    Create SenderTest.cpp

---
+++ Sender/SenderTest.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+
+#include<iostream>
+#include<string>
+#include"catch.hpp"
+#include"../Sender/Sender.h"
+using namespace std;
+
+CSVReader obj;
+
+TEST_CASE("When valid data file is received by the sender then print data successfully") {
+	string reqFileName = "Testdata/dataset.csv";
+	//string str = obj.getData(FileName);
+	REQUIRE(obj.setfilename == reqFileName);
+}
