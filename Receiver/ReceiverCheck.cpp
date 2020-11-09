3016634a6e71b9ec9e4247860e897fd2f16e5ff4
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-11-09 20:06:06 +0530

    Update ReceiverCheck.cpp

--- Receiver/ReceiverCheck.cpp
+++ Receiver/ReceiverCheck.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 //CSVReader reader("Testdata/dataset.csv");
 
-pair<vector<float>,vector<float>> ReceiverClass::dataFetch()
+//pair<vector<float>,vector<float>> ReceiverClass::dataFetch()
+void ReceiverClass::dataFetch()
 {
-  vector<float> data1;
-  vector<float> data2;
+  //vector<float> data1;
+  //vector<float> data2;
   
   //ifstream file(fileName);
+  ReceiverClass obj;
   string line = "";
   string temperature = "";
   string humidity = "";
@@ -20,10 +22,15 @@ pair<vector<float>,vector<float>> ReceiverClass::dataFetch()
     getline(str,temperature,',');
     getline(str,humidity,',');
     
-    data1.push_back(stof(temperature));
-    data2.push_back(stof(humidity));
+    //data1.push_back(stof(temperature));
+    //data2.push_back(stof(humidity));
+    obj.temperatureWarningLow(temperature);
+    obj.temperatureWarningHigh(temperature);
+    obj.temperatureErrorLow(temperature);
+    obj.temperatureErrorHigh(temperature);
+    obj.humidityWarning(humidity);
   }
-  return {data1,data2};
+  //return {data1,data2};
 }
   
 /*vector<float> ReceiverClass::humidityFetch()
@@ -52,17 +59,17 @@ pair<vector<float>,vector<float>> ReceiverClass::dataFetch()
 }*/
 ReceiverClass checker;
 
-auto res = checker.dataFetch();
+//auto res = checker.dataFetch();
 
-vector<float> temperature = res.first;
-vector<float> humidity = res.second;
+//vector<float> temperature = res.first;
+//vector<float> humidity = res.second;
 
-void ReceiverClass::temperatureWarningLow(vector<float> temperature)
+void ReceiverClass::temperatureWarningLow(float temperature)
 {
-  for(float temp:temperature)
-  {
-   checker.TempLow(temp);
-  }  
+  //for(float temp:temperature)
+  //{
+   checker.TempLow(temperature);
+  //}  
 }
 
 void ReceiverClass::TempLow(float temp)
@@ -75,12 +82,12 @@ void ReceiverClass::TempLow(float temp)
 }
   
   
-void ReceiverClass::temperatureWarningHigh(vector<float> temperature)
+void ReceiverClass::temperatureWarningHigh(float temperature)
 {
-  for(float temp:temperature)
-  {
-    checker.TempHigh(temp);
-  }
+  //for(float temp:temperature)
+  //{
+    checker.TempHigh(temperature);
+  //}
 }
 
 void ReceiverClass::TempHigh(float temp)
@@ -92,36 +99,36 @@ void ReceiverClass::TempHigh(float temp)
     }
 }
 
-void ReceiverClass::temperatureErrorLow(vector<float> temperature)
+void ReceiverClass::temperatureErrorLow(float temperature)
 {
-  for(float temp:temperature)
-  {
-    if(temp <= 0)
+  //for(float temp:temperature)
+  //{
+    if(temperature <= 0)
     {
-      cout << "Error: Current temperature is " << temp << " C" << endl;
+      cout << "Error: Current temperature is " << temperature << " C" << endl;
       checker.option = 2;
     }
-  }
+  //}
 }
 
-void ReceiverClass::temperatureErrorHigh(vector<float> temperature)
+void ReceiverClass::temperatureErrorHigh(float temperature)
 {
-  for(float temp:temperature)
-  {
-    if(temp >= 40)
+  //for(float temp:temperature)
+  //{
+    if(temperature >= 40)
     {
-      cout << "Error: Current temperature is " << temp << " C" << endl;
+      cout << "Error: Current temperature is " << temperature << " C" << endl;
       checker.option = 2;
     }
-  }
+  //}
 }
        
-void ReceiverClass::humidityWarning(vector<float> humidity)
+void ReceiverClass::humidityWarning(float humidity)
 {
-  for(float hum:humidity)
-  {
-    checker.HumidHigh(hum);
-  }
+  //for(float hum:humidity)
+  //{
+    checker.HumidHigh(humidity);
+  //}
 }
 
 void ReceiverClass::HumidHigh(float hum)
@@ -133,21 +140,22 @@ void ReceiverClass::HumidHigh(float hum)
     }
 }
        
-void ReceiverClass::humidityError(vector<float> humidity)
+void ReceiverClass::humidityError(float humidity)
 {
-  for(float hum:humidity)
-  {
-    if(hum >= 90)
+  //for(float hum:humidity)
+  //{
+    if(humidity >= 90)
     {
       cout<< "Error: Current Humidity is " << hum << " %" << endl;
       checker.option = 4;
     }
-  }
+  //}
 }
 
 int main()
 {
   ReceiverClass receiverObj;
+  receiverObj.dataFetch();
   //vector<float> data1 = receiverObj.temperatureFetch();
   //vector<float> data2 = receiverObj.humidityFetch();
   
@@ -161,7 +169,7 @@ int main()
   
   cout << endl;*/
   
-  if(temperature.size()!= 0)
+  /*if(temperature.size()!= 0)
   {
     receiverObj.temperatureWarningLow(temperature);
     receiverObj.temperatureWarningHigh(temperature);
@@ -184,5 +192,5 @@ int main()
   {
     cout<<"No data received"<<endl;
   }
-  return 0;
+  return 0;*/
 }
