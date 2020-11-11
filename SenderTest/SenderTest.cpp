27545ddd020050c93e122101d20484d9e080e269
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-11-11 21:14:18 +0530

    Update SenderTest.cpp

--- SenderTest/SenderTest.cpp
+++ SenderTest/SenderTest.cpp
@@ -9,14 +9,16 @@ using namespace std;
 CSVReader obj;
 
 
-TEST_CASE("When valid data file is received by the sender then print data successfully") {
+/*TEST_CASE("When valid data file is received by the sender then print data successfully") {
 	string reqFileName = "Testdata/dataset.csv";
 	//string str = obj.getData(FileName);
 	vector<vector<string>> data = obj.getData("Testdata/dataset.csv");
 	REQUIRE(setfilename == reqFileName);
-}
+}*/
 
-/*TEST_CASE("When a field is empty then the CSV file is invalid") {
-	string fileName = "Testdata/dummy.csv";
-*/	
+TEST_CASE("When a field is empty then the CSV file is invalid") {
 	
+	string fileName = "Testdata/dummy.csv";
+	vector<vector<string>> data = obj.getData(fileName);
+	REQUIRE(setfilename == "The CSV file has empty fields");
+}
