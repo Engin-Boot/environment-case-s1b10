591f77056addd3aeb72949c298a89d3a2b21b727
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-11-11 13:18:56 +0530

    Update ReceiverTest.cpp

--- ReceiverTest/ReceiverTest.cpp
+++ ReceiverTest/ReceiverTest.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 
 ReceiverClass obj;
-obj.temperatureWarningLow(3.7);
+//obj.temperatureWarningLow(3.7);
 //auto res = obj.dataFetch();
 
 //vector<float> temperature = res.first;
