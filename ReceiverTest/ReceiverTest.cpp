e4a55d67807964d4ca99540d3dbd0e50d8d601e6
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-11-11 11:31:04 +0530

    Update ReceiverTest.cpp

--- ReceiverTest/ReceiverTest.cpp
+++ ReceiverTest/ReceiverTest.cpp
@@ -12,15 +12,19 @@ using namespace std;
 //vector<float> temperature = res.first;
 //vector<float> humidity = res.second;
 
-vector<float> temperature = {10,20,30,40,50,60,2};
-vector<float> humidity = {80,90,30,40,97,60};
+//vector<float> temperature = {10,20,30,40,50,60,2};
+//vector<float> humidity = {80,90,30,40,97,60};
 
 TEST_CASE("Generate warning if Temperature >= 37 C or if Temperature <= 4 C")
 {
   ReceiverClass obj;
-  obj.temperatureWarningLow(temperature);
+  /*obj.temperatureWarningLow(temperature);
   obj.temperatureWarningHigh(temperature);
   //REQUIRE(obj.temperatureWarningLow(temperature) == ("Alert generated"));
+  REQUIRE(obj.option == 1);*/
+  
+  obj.temperatureWarningHigh(38.9);
+  obj.temperatureWarningLow(3.7);
   REQUIRE(obj.option == 1);
 }
 
