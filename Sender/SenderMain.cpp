9549216e5b3370e61a48e41de4ed84cadf2ef6f2
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-11-13 20:14:08 +0530

    Update SenderMain.cpp

--- Sender/SenderMain.cpp
+++ Sender/SenderMain.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 #include<vector>
 #include<string>
-#include<unistd.h>
+//#include<unistd.h>
 //string setfilename;
 
 int main()
@@ -23,16 +23,16 @@ int main()
     
     reader.extractRow(dataList);
     
-    while(1)
-    {
+   // while(1)
+   // {
         if(setfilename == "The CSV file has empty fields") /*|| setfilename == "The CSV file has invalid data")*/
             cout << setfilename << endl;
 
         else
             reader.printData(dataList);
         
-        usleep(300);
-    }
+    //    usleep(300);
+    //}
     
         
    /* for(vector<string> vec : dataList)
