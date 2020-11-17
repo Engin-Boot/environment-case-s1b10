7e4798a79f8681c8bfee402d3feecc43880a37f9
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-11-17 20:41:00 +0530

    Update SenderCheck.cpp

--- Sender/SenderCheck.cpp
+++ Sender/SenderCheck.cpp
@@ -22,40 +22,23 @@ void CSVReader::printData(vector<vector<string> > dataList)
      }
 }
 
-/*bool checkValidity(string data)
-{
-	cout << data << " " <<endl;
-   for(unsigned int i = 0; i < data.length(); i++)
-	{
-		if ((isdigit((data[i])) == 0) && (data[i] != '-') && (data[i] != '\n'))
-			return false;
-	}	
-	return true; 
-}*/
 
 void setCondition(vector<string> row)
 {
     for(string data : row)
     {
-	//data.erase(remove(data.begin(), data.end(), '\n'), data.end());
-	//cout << data << "value" << endl;
-	//cout << "result: " << isdigit('2') <<endl;
+	
 	if(data == "")
 	{
 	   setfilename = "The CSV file has empty fields";
-	   //cout << "1st if" << endl;
 	}
-	/*if(!checkValidity(data))
-	{
-	   setfilename = "The CSV file has invalid data";
-	   cout << "2nd if" << endl;
-	}*/
+	
     }
 }	   
 
 void CSVReader::extractRow(vector<vector<string> > dataList)
 {
-    //cout << dataList << endl;
+    
     for(vector<string> vec : dataList)
     {
 	setCondition(vec);
@@ -83,22 +66,8 @@ vector<vector<string> > CSVReader::getData(string filename)
 
         {
             
-          /*  if(data == "")
-            {
-                setfilename = "The CSV file has empty fields";
-                exit(-1);
-            }
-            
-            else if(!checkValidity(data))
-            {
-                setfilename = "The CSV file has invalid data";
-                exit(-2);
-            }
-            
-            else
-            {*/
                 vec.push_back(data);
-            //}
+            
         }
         dataList.push_back(vec);
     }
@@ -106,82 +75,3 @@ vector<vector<string> > CSVReader::getData(string filename)
     return dataList;
 }
 
-/*vector<float> CSVReader::temperatureFetch()
-{
-  vector<float> data1;
-  
-  ifstream file(fileName);
-  string line = "";
-  string temperature = "";
-  
-  getline(file,line);
-  while(getline(file,line))
-  {
-    stringstream str(line);
-    
-    getline(str,temperature,',');
-    
-    data1.push_back(stof(temperature));
-    
-  }
-  return data1;
-}
-  
-vector<float> CSVReader::humidityFetch()
-{
-  vector<float> data2;
-  ifstream file(fileName);
-  string line = "";
-  string humidity = "";
-  
-  getline(file,line);
-  while(getline(file,line))
-  {
-    stringstream str(line);
-    
-    getline(str,humidity,',');
-    getline(str,humidity,',');
-    
-    data2.push_back(stof(humidity));
-  }
-  return data2;
-}*/
-
-
-/*int main()
-{
-    
-    //CSVReader reader("Testdata/dataset.csv");
-    CSVReader reader;
-    vector<vector<string> > dataList = reader.getData("Testdata/dataset.csv");
-    
-    for(vector<string> vec : dataList)
-    {
-        for(string data : vec)
-        {
-            cout << stof(data) << " , ";
-        }
-        cout << endl;
-    }*/
-    
-  /*  vector<float> temperature = reader.temperatureFetch();
-    vector<float> humidity = reader.humidityFetch();
-    
-    for(float t1 : temperature && float h1 : humidity)
-    {
-        cout << t1 << " , " << h1 << endl;
-    }
-    
-    vector<float> result;
-    
-    result.reserve( temperature.size() + humidity.size() );
-    result.insert( result.end(), temperature.begin(), temperature.end() );
-    result.insert( result.end(), humidity.begin(), humidity.end() );
-    
-    for(float i : result)
-    {
-        cout << i << endl ;
-    }*/
-    
-   // return 0;
-//}
