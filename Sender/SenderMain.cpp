656685cb673553d1746f67cf734c3da914ddfece
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-11-17 20:36:50 +0530

    Update SenderMain.cpp

--- Sender/SenderMain.cpp
+++ Sender/SenderMain.cpp
@@ -2,47 +2,23 @@
 #include<iostream>
 #include<vector>
 #include<string>
-//#include<unistd.h>
-//string setfilename;
 
 int main()
 {
     
-    //CSVReader reader("Testdata/dataset.csv");
     CSVReader reader;
     vector<vector<string> > dataList = reader.getData("Testdata/dataset.csv");
     
-    /*for(vector<string> vec : dataList)
-    {
-        for(string data : vec)
-        {
-            cout << stof(data) << " , ";
-        }
-        //cout << endl;
-    }*/
-    
+        
     reader.extractRow(dataList);
     
-   // while(1)
-   // {
-        if(setfilename == "The CSV file has empty fields") /*|| setfilename == "The CSV file has invalid data")*/
-            cout << setfilename << endl;
+   
+    if(setfilename == "The CSV file has empty fields")
+        cout << setfilename << endl;
 
-        else
-            reader.printData(dataList);
+    else
+        reader.printData(dataList);
         
-    //    usleep(300);
-    //}
-    
-        
-   /* for(vector<string> vec : dataList)
-    {
-        for(string data : vec)
-        {
-            cout << stof(data) << " , ";
-        }
-        cout << endl;
-    }*/
     
     return 0;
  }
