0c95c3d9314cde74dca6100ea4d970633637ee7e
sandeepprusty10 <44285578+sandeepprusty10@users.noreply.github.com>
2020-11-17 20:29:07 +0530

    Update ReceiverCheck.cpp

--- Receiver/ReceiverCheck.cpp
+++ Receiver/ReceiverCheck.cpp
@@ -1,18 +1,12 @@
 #include "../Receiver/ReceiverCheck.h"
 using namespace std;
 
-//CSVReader reader("Testdata/dataset.csv");
-
-//pair<vector<float>,vector<float>> ReceiverClass::dataFetch()
 
 int option = 0;
 
 void ReceiverClass::dataFetch()
 {
-  //vector<float> data1;
-  //vector<float> data2;
   
-  //ifstream file(fileName);
   ReceiverClass obj;
   string line = "";
   string temperature = "";
@@ -25,8 +19,6 @@ void ReceiverClass::dataFetch()
     getline(str,temperature,',');
     getline(str,humidity,',');
     
-    //data1.push_back(stof(temperature));
-    //data2.push_back(stof(humidity));
     obj.temperatureWarningLow(stof(temperature));
     obj.temperatureWarningHigh(stof(temperature));
     obj.temperatureErrorLow(stof(temperature));
@@ -34,46 +26,18 @@ void ReceiverClass::dataFetch()
     obj.humidityWarning(stof(humidity));
     obj.humidityError(stof(humidity));
   }
-  //return {data1,data2};
+  
 }
   
-/*vector<float> ReceiverClass::humidityFetch()
-{
-  vector<float> data2;
-  //ifstream file(fileName);
-  string line = "";
-  string humidity = "";
-  
-  while(getline(cin,line))
-  {
-    stringstream str(line);
-    
-    getline(str,humidity,',');
-    getline(str,humidity,',');
-    
-    data2.push_back(stof(humidity));
-  }
-  return data2;
-}*/
 
-/*bool CheckData(vector<float> value)
-{
-    bool isDataReceived = getline(cin, value).good();
-    return isDataReceived;
-}*/
 ReceiverClass checker;
 
-//auto res = checker.dataFetch();
-
-//vector<float> temperature = res.first;
-//vector<float> humidity = res.second;
 
 void ReceiverClass::temperatureWarningLow(float temperature)
 {
-  //for(float temp:temperature)
-  //{
+  
    checker.TempLow(temperature);
-  //}  
+    
 }
 
 void ReceiverClass::TempLow(float temp)
@@ -88,10 +52,9 @@ void ReceiverClass::TempLow(float temp)
   
 void ReceiverClass::temperatureWarningHigh(float temperature)
 {
-  //for(float temp:temperature)
-  //{
+  
     checker.TempHigh(temperature);
-  //}
+  
 }
 
 void ReceiverClass::TempHigh(float temp)
@@ -105,34 +68,31 @@ void ReceiverClass::TempHigh(float temp)
 
 void ReceiverClass::temperatureErrorLow(float temperature)
 {
-  //for(float temp:temperature)
-  //{
+  
     if(temperature <= 0)
     {
       cout << "Error: Current temperature is " << temperature << " C" << endl;
       option = 2;
     }
-  //}
+  
 }
 
 void ReceiverClass::temperatureErrorHigh(float temperature)
 {
-  //for(float temp:temperature)
-  //{
+  
     if(temperature >= 40)
     {
       cout << "Error: Current temperature is " << temperature << " C" << endl;
       option = 2;
     }
-  //}
+  
 }
        
 void ReceiverClass::humidityWarning(float humidity)
 {
-  //for(float hum:humidity)
-  //{
+  
     checker.HumidHigh(humidity);
-  //}
+  
 }
 
 void ReceiverClass::HumidHigh(float hum)
@@ -146,52 +106,12 @@ void ReceiverClass::HumidHigh(float hum)
        
 void ReceiverClass::humidityError(float humidity)
 {
-  //for(float hum:humidity)
-  //{
+  
     if(humidity >= 90)
     {
       cout<< "Error: Current Humidity is " << humidity << " %" << endl;
       option = 4;
     }
-  //}
+  
 }
-
-
-  //vector<float> data1 = receiverObj.temperatureFetch();
-  //vector<float> data2 = receiverObj.humidityFetch();
-  
-  /*for(float i : temperature)
-    cout << i << " ";
-  
-  cout << endl;
-  
-  for(float j : humidity)
-    cout << j << " ";
-  
-  cout << endl;*/
-  
-  /*if(temperature.size()!= 0)
-  {
-    receiverObj.temperatureWarningLow(temperature);
-    receiverObj.temperatureWarningHigh(temperature);
-    receiverObj.temperatureErrorHigh(temperature);
-    receiverObj.temperatureErrorLow(temperature);
-      
-  }
-  else
-  {
-    cout<<"No data received"<<endl;
-  }
- 
-  if(humidity.size()!= 0)
-  {
-    receiverObj.humidityWarning(humidity);
-    receiverObj.humidityError(humidity);
-  }
-    
-  else
-  {
-    cout<<"No data received"<<endl;
-  }
-  return 0;*/
 
